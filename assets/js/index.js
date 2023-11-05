ba2be822ea29265771a649476ef47c3a79bf1f97
Esteban Martinez <camilo.martinezmma@cun.edu.co>
2023-11-05 13:09:01 -0500

    segunda version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
-console.log(first)
+
+const menu = document.querySelector('.header__menu')
+const headerNav = document.querySelector('.header__nav')
+
+headerNav.addEventListener('click', function (e) {
+
+    if (e.target.closest('.btn--open')) {
+
+        menu.classList.toggle('show--menu')
+
+    }
+
+    if (e.target.closest('.btn--close')) {
+        
+        menu.classList.remove('show--menu')
+
+    }
+
+})
+
+
+
+
